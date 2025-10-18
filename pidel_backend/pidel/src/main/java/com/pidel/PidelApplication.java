0f4c60f9b8afd43f28ead7920fc1da6531b669b3
generalsuslik <glebrusyaev77@gmail.com>
2025-10-18 13:17:22 +0300

    Init

---
+++ pidel_backend/pidel/src/main/java/com/pidel/PidelApplication.java
@@ -0,0 +1,13 @@
+package com.pidel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PidelApplication.class, args);
+    }
+
+}
---
+++ pidel_backend/pidel/src/test/java/com/pidel/PidelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pidel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PidelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
